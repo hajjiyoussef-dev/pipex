0115431be2863208e87b6fdd8bcc1f557ea3fca4
hajjiyoussef-dev <hajjiyoussef016@gmail.com>
2025-03-03 17:41:42 +0000

    feat: add get_next_line functin

--- bonus /pipex_bonus.h
+++ bonus /pipex_bonus.h
@@ -6,22 +6,32 @@
 /*   By: yhajji <yhajji@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 19:31:21 by yhajji            #+#    #+#             */
-/*   Updated: 2025/03/02 23:23:34 by yhajji           ###   ########.fr       */
+/*   Updated: 2025/03/03 17:41:10 by yhajji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_BONUS_H
 #define PIPEX_BONUS_H
 
+
+#define BUFFER_SIZE 42
+
 // delete ???
 #include <stdio.h>
 
 #include <unistd.h>
+#include <limits.h>
 #include <stdlib.h>
 #include <fcntl.h>
 
 
 void ft_error(char *str);
+
+void ft_execute_cmd(char *argv, char **ev);
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strdup(const char *s1);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 
 
 #endif
